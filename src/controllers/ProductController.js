2bc71e39adf8b2098f3f9bdb5f514bec4f3b0ee2
imThiTran <tranvanthitk113@gmail.com>
2021-08-31 14:17:11 +0700

    sua loi tim kiem di qua chi tiet

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -20,19 +20,7 @@ class ProductControler{
             })
         })                
     }
-    search(req,res){
-        var key = req.query.product;
-        var sql = `SELECT * FROM products where Id not in (select Id from products where CategoryId = 6 or CategoryId = 7)`;
-        con.query(sql, (err,results) => {
-            if(err) throw err;
-            var newResults = results.filter(function(result){
-                return result.Name.toLowerCase().indexOf(key.toLowerCase()) !== -1;
-            })
-            res.render('product/show',{
-                products : newResults             
-            })
-        })
-    }
+    
 }
 
 module.exports = new ProductControler
--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -5,7 +5,19 @@ class SiteControler{
             res.render('home')
        }
 
-    
+       search(req,res){
+        var key = req.query.product;
+        var sql = `SELECT * FROM products where Id not in (select Id from products where CategoryId = 6 or CategoryId = 7)`;
+        con.query(sql, (err,results) => {
+            if(err) throw err;
+            var newResults = results.filter(function(result){
+                return result.Name.toLowerCase().indexOf(key.toLowerCase()) !== -1;
+            })
+            res.render('product/show',{
+                products : newResults             
+            })
+        })
+    }
     
 }
 
